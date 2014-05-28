4cac8123f5d0d400c4596d635b06235e5a793248
bobby <xaevir@gmail.com>
2014-05-27 22:24:05 -0400

    added logger

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var serve = require('koa-static');
 var betterBody = require('koa-better-body')
 var views = require('koa-views')
 
+app.use(logger());
 app.use(serve(__dirname + '/public'));
 app.use(betterBody({patchNode: false, jsonLimit: '1kb', formLimit: '1kb'}));
 app.use(views('views', { default: 'jade'}));
