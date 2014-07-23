f8577ab5b07b546accfe8401e15a01fd859ad743
bobby <xaevir@gmail.com>
2014-07-23 16:55:22 -0400

    optimized dog image

---
+++ app/js/app.js
@@ -0,0 +1,44 @@
+'use strict';
+
+angular.module('pretzelApp', ['pretzelApp.Steps', 'ngNotify', 'rcForm', 'ngSanitize'])
+  .controller('pretzelCtrl', function ($scope, $http, $filter) {
+
+    $scope.pageElements = [];
+
+    $http.get('/js/data.json')
+      .success(function (data) {
+        $scope.pageElements = data;
+        $scope.pageElements.supportGroups = $filter('group')(data.support, 4);
+        $scope.pageElements.moreSupportGroups = $filter('group')(data.moreSupport, 4);
+      })
+      .error(function (error) {
+        $scope.error = error;
+      });
+  })
+  .controller('requestFormCtrl', function ($scope, $http, ngNotify) {
+    
+    $scope.data = {country: 1};
+
+    $scope.resetData = function() {
+      $scope.data = {country: 1};
+    };
+
+    $scope.sendData = function (data) {
+      $http.post('/request-info', data)
+        .success(function (res) {
+          ngNotify.set('Thank you for your interest! We have received your request and will get back to you shortly.', {
+              position: 'top',
+          });
+          $scope.requestForm.$setPristine();
+          $scope.resetData();
+          $scope.form.reset();
+        })
+      .error(function (error) {
+         ngNotify.set('There was an error. Please try again.', 'error');
+      });
+    };
+
+
+  });
+
+
